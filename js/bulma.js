898b37d4b734286ecd71678c760340ad8ce1f237
aulianafahrian <aulianafahrian@gmail.com>
2023-10-07 13:44:22 +0700

    push

--- js/bulma.js
+++ js/bulma.js
@@ -1,4 +1,4 @@
-// The following code is based off a toggle menu by @Bradcomp
+// // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 // (function() {
 //     var burger = document.querySelector('.burger');
--- js/tabs.js
+++ js/tabs.js
@@ -26,31 +26,31 @@
 //     });
 //   }
 
-//   document.querySelectorAll("#nav li").forEach(navEl => {
-//     navEl.addEventListener("click", () => toggleTab(navEl.id, navEl.dataset.target));
-// });
+
+document.querySelectorAll("#nav li").forEach(function(navEl) {
+  navEl.onclick = function() { toggleTab(this.id, this.dataset.target); }
+});
 
 function toggleTab(selectedNav, targetId) {
-    const navEls = document.querySelectorAll("#nav li");
+  var navEls = document.querySelectorAll("#nav li");
 
-    navEls.forEach(navEl => {
-        if (navEl.id == selectedNav) {
-            navEl.classList.add("is-active");
-        } else {
-            if (navEl.classList.contains("is-active")) {
-                navEl.classList.remove("is-active");
-            }
-        }
-    });
+  navEls.forEach(function(navEl) {
+    if (navEl.id == selectedNav) {
+      navEl.classList.add("is-active");
+    } else {
+      if (navEl.classList.contains("is-active")) {
+        navEl.classList.remove("is-active");
+      }
+    }
+  });
 
-    const tabs = document.querySelectorAll(".tab-pane");
+  var tabs = document.querySelectorAll(".tab-pane");
 
-    tabs.forEach(tab => {
-        if (tab.id == targetId) {
-            tab.style.display = "block";
-        } else {
-            tab.style.display = "none";
-        }
-    });
+  tabs.forEach(function(tab) {
+    if (tab.id == targetId) {
+      tab.style.display = "block";
+    } else {
+      tab.style.display = "none";
+    }
+  });
 }
-
