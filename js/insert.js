7fd75180864d7bc4e851e476f9b1d0cf6013cbce
erditona <erditonaushaadam@gmail.com>
2023-10-07 15:39:54 +0700

    push dito

--- js/insert.js
+++ js/insert.js
@@ -1,2 +1,5 @@
+import data from "../json/isiproposal.json" assert { type: "json" };
 import { setInner } from "https://jscroot.github.io/element/croot.js";
-setInner("demo", "Dari croot.js");
+
+setInner("judulProposal", data.title);
+setInner("deskripsiProposal", data.description);
